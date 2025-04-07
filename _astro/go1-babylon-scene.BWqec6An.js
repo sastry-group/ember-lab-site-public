ace33396f4bebbf46966a606ac59693f198f4da3
Thomas Kragerud <thomas.kragerud@gmail.com>
2025-04-06 22:28:00 -0700

    Removed all

--- _astro/go1-babylon-scene.BWqec6An.js
+++
@@ -1,1 +0,0 @@
-import{y as a}from"./go1-babylon-scene.CW5ApeTL.js";import"./index.Dy6lLLXr.js";export{a as default};
--- _astro/tgaTextureLoader.Du738qSl.js
+++
@@ -1,1 +0,0 @@
-import{h as r,i as n}from"./go1-babylon-scene.CW5ApeTL.js";import"./index.Dy6lLLXr.js";class d{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,a){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(o);a(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{d as _TGATextureLoader};
